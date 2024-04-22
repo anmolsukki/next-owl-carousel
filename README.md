ffb8698941db3e46a139ef0980719c88dd95f84b
Anmol Kumar Singh <anmolsukki94@gmail.com>
2024-04-22 15:26:56 +0530

    Document Update

--- README.md
+++ README.md
@@ -1,5 +1,89 @@
-### Next Owl Carousel
+## Next Owl Carousel
+
+#### 1.Installation
+
+```script
+npm install --save next-owl-carousel
+```
 
 ```html
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
 ```
+
+```jsx
+import { useRef } from 'react';
+import OwlCarousel from 'next-owl-carousel';
+```
+
+### 2.Usage
+
+```jsx
+const carouselRef = useRef(null);
+```
+
+```jsx
+const options = {
+  loop: true,
+  responsiveClass: true,
+  dots: false,
+  autoplay: true,
+  responsive: {
+    0: { items: 1, dots: true },
+    768: { items: 2 },
+  },
+};
+```
+
+```jsx
+const events = {
+  onDragged: function (event) {
+    console.log('====onDragged==>>', event);
+  },
+  onChanged: function (event) {
+    console.log('====onChanged==>>', event);
+  },
+};
+```
+
+```jsx
+<OwlCarousel ref={carouselRef} options={options} events={events} className="owl-carousel">
+  <div className="item"><img src="/img/fullimage1.jpg" alt="item1" /></div>
+  <div className="item"><img src="/img/fullimage1.jpg" alt="item2" /></div>
+  <div className="item"><img src="/img/fullimage1.jpg" alt="item3" /></div>
+</OwlCarousel>
+```
+
+### 3. Methods
+
+<u>Previous</u>
+
+```jsx
+<button onClick={() => carouselRef?.current?.prev()}>prev</button>
+```
+
+<u>Next</u>
+
+```jsx
+<button onClick={() => carouselRef?.current?.next()}>prev</button>
+```
+
+<u>goTo(page)</u>
+
+```jsx
+<button onClick={() => carouselRef?.current?.goTo(0)}>prev</button>
+```
+
+#### Reference https://www.npmjs.com/package/react-owl-carousel2
+
+---
+
+### Hi there, I'm [Anmol](https://www.linkedin.com/in/anmolsukki/) 👋
+
+[![CodeSandbox](https://img.shields.io/badge/Codesandbox-000000?style=flat-round&logo=CodeSandbox)](https://codesandbox.io/u/anmolsukki)&nbsp;&nbsp;&nbsp;
+[![Discord](https://img.shields.io/badge/Discord-%235865F2.svg?style=flat-round&logo=discord&logoColor=white)](https://discord.gg/zMkSphwHjE)&nbsp;&nbsp;&nbsp;
+[![Linkedin](https://img.shields.io/badge/-LinkedIn-blue?style=flat-round&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/anmolsukki/)](https://www.linkedin.com/in/anmolsukki/)&nbsp;&nbsp;&nbsp;
+[![facebook](https://aleen42.github.io/badges/src/facebook.svg)](https://www.facebook.com/Anmolsukki/)&nbsp;&nbsp;&nbsp;
+[![twitter](https://aleen42.github.io/badges/src/twitter.svg)](https://twitter.com/anmolsukki)&nbsp;&nbsp;&nbsp;
+[![Instagram](https://img.shields.io/badge/-Instagram-e4405f?style=flat-round&logo=Instagram&logoColor=white)](https://www.instagram.com/anmolsukki/)&nbsp;&nbsp;&nbsp;
+[![slack](https://aleen42.github.io/badges/src/slack.svg)](https://join.slack.com/t/anmolsukki/shared_invite/zt-k7cfber5-JVl_kGaNdNqvwsMADPiUWg)&nbsp;&nbsp;&nbsp;
+[![stackoverflow](https://aleen42.github.io/badges/src/stackoverflow.svg)](https://stackoverflow.com/users/10825957/anmol-kumar-singh)
--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function _isNativeReflectConstruct() { return !!t; })(); }
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var owlCarouselOptions = {
   core: ['items', 'loop', 'center', 'rewind', 'mouseDrag', 'touchDrag', 'pullDrag', 'freeDrag', 'margin', 'stagePadding', 'merge', 'mergeFit', 'autoWidth', 'startPosition', 'rtl', 'smartSpeed', 'fluidSpeed', 'dragEndSpeed', 'responsive', 'responsiveRefreshRate', 'responsiveBaseElement', 'fallbackEasing', 'info', 'nestedItemSelector', 'itemElement', 'stageElement', 'refreshClass', 'loadedClass', 'loadingClass', 'rtlClass', 'responsiveClass', 'dragClass', 'itemClass', 'stageClass', 'stageOuterClass', 'grabClass'],
@@ -45,12 +45,11 @@ var owlCarouselEvents = {
   video: ['onStopVideo', 'onPlayVideo']
 };
 var OwlCarousel = /*#__PURE__*/function (_React$Component) {
-  _inherits(OwlCarousel, _React$Component);
   function OwlCarousel(props) {
     var _this;
     _classCallCheck(this, OwlCarousel);
     _this = _callSuper(this, OwlCarousel, [props]);
-    _defineProperty(_assertThisInitialized(_this), "onTranslate", function (next) {
+    _defineProperty(_this, "onTranslate", function (next) {
       return function (event) {
         _this.currentPosition = event.item.index;
         if (next) next(event);
@@ -66,10 +65,11 @@ var OwlCarousel = /*#__PURE__*/function (_React$Component) {
       return _this.$car.to(x);
     };
     _this.currentPosition = 0;
-    _this.onTranslate = _this.onTranslate.bind(_assertThisInitialized(_this));
+    _this.onTranslate = _this.onTranslate.bind(_this);
     return _this;
   }
-  _createClass(OwlCarousel, [{
+  _inherits(OwlCarousel, _React$Component);
+  return _createClass(OwlCarousel, [{
     key: "componentDidMount",
     value: function componentDidMount() {
       require('./owl.carousel.js');
@@ -145,7 +145,6 @@ var OwlCarousel = /*#__PURE__*/function (_React$Component) {
       }, props), children);
     }
   }]);
-  return OwlCarousel;
 }(_react["default"].Component);
 OwlCarousel.propTypes = {
   children: _propTypes["default"].oneOfType([_propTypes["default"].element, _propTypes["default"].arrayOf(_propTypes["default"].element.isRequired)]).isRequired,
